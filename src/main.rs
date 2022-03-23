38e40cc9c9faf3e2497c7ab13638c7aac265b17f
Jiajun Xie <jx31@st-andrews.ac.uk>
2022-03-23 01:13:44 +0000

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use colored::Colorize;
+use exitfailure::ExitFailure;
+use failure::ResultExt;
+use std::io::{self, Read};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(default_value = "ニャーニャー！")]
+    /// What does the cat say?
+    message: String,
+
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Load the cat picture from the specified file
+    catfile: Option<std::path::PathBuf>,
+
+    #[structopt(short = "i", long = "stdin")]
+    /// Read the message from STDIN instead of the argument
+    stdin: bool,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let options = Options::from_args();
+    let mut message: String = String::new();
+
+    if options.stdin {
+        io::stdin().read_to_string(&mut message)?;
+    } else {
+        message = options.message
+    }
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like dog.")
+    }
+
+    // println!("{}", message.bright_yellow().underline().on_purple());
+
+    let eye = if options.dead { "x" } else { "o" };
+    println!("{message}");
+    match &options.catfile {
+        Some(path) => {
+            let cat_template = std::fs::read_to_string(path)
+                .with_context(|_| format!("could not read the file {path:?}"))?;
+            let cat_picture = cat_template.replace("{eye}", eye);
+            println!("{}", &cat_picture);
+            Ok(())
+        }
+        None => {
+            println!(" \\");
+            println!("  \\");
+            println!("    /\\_/\\");
+            println!("   ( {eye} {eye} )", eye = eye.red().bold());
+            println!("   =( I )=");
+            Ok(())
+        }
+    }
 }
